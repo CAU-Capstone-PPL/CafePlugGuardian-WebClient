0414ecec49e066f35b80fa485ecdb7d09c00453d
manleKim <kk410910@naver.com>
2023-11-12 14:52:17 +0900

    feat: 플러그 상세 정보 api test code 추가

---
+++ lib/services/data_test.dart
@@ -0,0 +1,145 @@
+const List<Map<String, dynamic>> dummyDataPlugDetail = [
+  {
+    'plugId': '1',
+    'plugName': 'Plug 1',
+    'onOff': 'On',
+    'startTime': '08:00 AM',
+    'runningTime': '2',
+    'usedPower': '100',
+    'assignPower': '150',
+    'plugDescription': 'hello 1',
+    'realTimePower': '200'
+  },
+  {
+    'plugId': '2',
+    'plugName': 'Plug 2',
+    'onOff': 'Off',
+    'startTime': '',
+    'runningTime': '',
+    'usedPower': '',
+    'assignPower': '',
+    'plugDescription': 'hello 2',
+    'realTimePower': ''
+  },
+  {
+    'plugId': '3',
+    'plugName': 'Plug 3',
+    'onOff': 'On',
+    'startTime': '03:45 PM',
+    'runningTime': '3',
+    'usedPower': '120',
+    'assignPower': '200',
+    'plugDescription': 'hello 3',
+    'realTimePower': '50'
+  },
+  {
+    'plugId': '4',
+    'plugName': 'Plug 4',
+    'onOff': 'On',
+    'startTime': '09:15 AM',
+    'runningTime': '1.5',
+    'usedPower': '80',
+    'assignPower': '110',
+    'plugDescription': 'hello 4',
+    'realTimePower': '60'
+  },
+  {
+    'plugId': '5',
+    'plugName': 'Plug 5',
+    'onOff': 'Off',
+    'startTime': '',
+    'runningTime': '',
+    'usedPower': '',
+    'assignPower': '',
+    'plugDescription': 'hello 5',
+    'realTimePower': ''
+  },
+  {
+    'plugId': '6',
+    'plugName': 'Plug 6',
+    'onOff': 'On',
+    'startTime': '04:30 PM',
+    'runningTime': '2',
+    'usedPower': '110',
+    'assignPower': '150',
+    'plugDescription': 'hello 6',
+    'realTimePower': '80'
+  },
+  {
+    'plugId': '7',
+    'plugName': 'Plug 7',
+    'onOff': 'On',
+    'startTime': '11:45 AM',
+    'runningTime': '2.5',
+    'usedPower': '95',
+    'assignPower': '130',
+    'plugDescription': 'hello 7',
+    'realTimePower': '120'
+  },
+  {
+    'plugId': '8',
+    'plugName': 'Plug 8',
+    'onOff': 'Off',
+    'startTime': '',
+    'runningTime': '',
+    'usedPower': '',
+    'assignPower': '',
+    'plugDescription': 'hello 8',
+    'realTimePower': ''
+  },
+  {
+    'plugId': '9',
+    'plugName': 'Plug 9',
+    'onOff': 'On',
+    'startTime': '06:00 AM',
+    'runningTime': '4',
+    'usedPower': '150',
+    'assignPower': '180',
+    'plugDescription': 'hello 9',
+    'realTimePower': '210'
+  },
+  {
+    'plugId': '10',
+    'plugName': 'Plug 10',
+    'onOff': 'Off',
+    'startTime': '',
+    'runningTime': '',
+    'usedPower': '',
+    'assignPower': '',
+    'plugDescription': 'hello 10',
+    'realTimePower': ''
+  },
+  {
+    'plugId': '11',
+    'plugName': 'Plug 11',
+    'onOff': 'On',
+    'startTime': '07:30 AM',
+    'runningTime': '3',
+    'usedPower': '120',
+    'assignPower': '160',
+    'plugDescription': 'hello 11',
+    'realTimePower': '211W'
+  },
+  {
+    'plugId': '12',
+    'plugName': 'Plug 12',
+    'onOff': 'On',
+    'startTime': '10:00 AM',
+    'runningTime': '1.5',
+    'usedPower': '70',
+    'assignPower': '100',
+    'plugDescription': 'hello 12',
+    'realTimePower': '223W'
+  },
+  {
+    'plugId': '13',
+    'plugName': 'Plug 13',
+    'onOff': 'Off',
+    'startTime': '',
+    'runningTime': '',
+    'usedPower': '',
+    'assignPower': '',
+    'plugDescription': 'hello 13',
+    'realTimePower': ''
+  }
+];
